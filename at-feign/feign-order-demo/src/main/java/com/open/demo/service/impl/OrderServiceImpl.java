2d40884bd46bce18ac2046673736ecc7e1c9dc26
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-10-10 17:49:08 +0800

    style: SpringCloud事务-配置feign降级,事务失效解决方案-手动事务提交

--- at-feign/feign-order-demo/src/main/java/com/open/demo/service/impl/OrderServiceImpl.java
+++ at-feign/feign-order-demo/src/main/java/com/open/demo/service/impl/OrderServiceImpl.java
@@ -8,7 +8,9 @@ import com.open.demo.mapper.OrderMapper;
 import com.open.demo.pojo.OrderDO;
 import com.open.demo.service.OrderService;
 import io.seata.core.context.RootContext;
+import io.seata.core.exception.TransactionException;
 import io.seata.spring.annotation.GlobalTransactional;
+import io.seata.tm.api.GlobalTransactionContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,7 +33,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     @GlobalTransactional
-    public Integer createOrder(Long userId, Long productId, Integer price) {
+    public Integer createOrder(Long userId, Long productId, Integer price) throws TransactionException {
 
         // 购买数量，暂时设置为 1
         Integer amount = 1;
@@ -43,6 +45,11 @@ public class OrderServiceImpl implements OrderService {
 
         // 扣减余额
         accountService.reduceBalance(new AccountReduceBalanceDTO().setUserId(userId).setPrice(price));
+        // 通过 accountService 服务的降级返回 手动回滚事务 假设返回false
+        if (false) {
+            //手动回滚
+            GlobalTransactionContext.reload(RootContext.getXID()).rollback();
+        }
 
         // 保存订单
         OrderDO order = new OrderDO().setUserId(userId).setProductId(productId).setPayAmount(amount * price);
